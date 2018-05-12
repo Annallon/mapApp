da1005104d91f308e3cd437ce4ae174b5a809b1d
marty-crane <kyam.lh@googlemail.com>
2018-05-12 13:11:38 +0100

    user input validated

---
+++ assets/js/getAddress.js
@@ -0,0 +1,20 @@
+document.querySelector('#addNewUser').addEventListener('submit', function(event) {
+    event.preventDefault()
+    var addressField = document.getElementById("location")
+    var addressValid;
+    if(isPostcodeValid(addressField.value)){
+        addressValid = true;
+    } else {
+        addressValid = false;
+    }
+    console.log(addressValid);
+});
+
+
+function isPostcodeValid(postcode) {
+    const regexPostcode = /^(([gG][iI][rR] {0,}0[aA]{2})|(([aA][sS][cC][nN]|[sS][tT][hH][lL]|[tT][dD][cC][uU]|[bB][bB][nN][dD]|[bB][iI][qQ][qQ]|[fF][iI][qQ][qQ]|[pP][cC][rR][nN]|[sS][iI][qQ][qQ]|[iT][kK][cC][aA]) {0,}1[zZ]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yxA-HK-XY]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/
+    if (regexPostcode.test(postcode)) {
+        return true
+    }
+    return false
+}
