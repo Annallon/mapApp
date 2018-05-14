b0d588cf2d8d77e3d45a648665fa9a48f013c843
Annallon <kaediharris@gmail.com>
2018-05-14 14:52:14 +0100

    Trying a few thigns to test if item is in array..WIP

--- assets/js/getAddress.js
+++ assets/js/getAddress.js
@@ -4,7 +4,7 @@ document.querySelector('#addNewUser').addEventListener('submit', function(event)
     event.preventDefault()
     var addressField = document.getElementById("location")
 
-    if(isPostcodeValid(addressField.value)){
+    if((isPostcodeValid(addressField.value)) && (checkIfAlreadyAdded(userArray, addressField.value))){
         codeAddress(addressField.value, function(array){
             userArray.push(array)
             console.log(userArray)  
@@ -16,23 +16,8 @@ document.querySelector('#addNewUser').addEventListener('submit', function(event)
     }
 
 
-    // codeAddress(addressField.value, function(array){
-    //     userArray.push(array)
-    //     console.log(userArray)  
-    // });
-    
-    // async function callingAPI(addressField) {
-    
-    // var foo = await codeAddress(addressField.value);
-    // console.log(foo);
-    // }
 
-    //callingAPI(addressField);
-    // codeAddress(addressField.value, function(results, status) {
-    //     console.log.
-    // })
 });
-
 
 function isPostcodeValid(postcode) {
     const regexPostcode = /^(([gG][iI][rR] {0,}0[aA]{2})|(([aA][sS][cC][nN]|[sS][tT][hH][lL]|[tT][dD][cC][uU]|[bB][bB][nN][dD]|[bB][iI][qQ][qQ]|[fF][iI][qQ][qQ]|[pP][cC][rR][nN]|[sS][iI][qQ][qQ]|[iT][kK][cC][aA]) {0,}1[zZ]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yxA-HK-XY]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/
@@ -41,6 +26,27 @@ function isPostcodeValid(postcode) {
     }
     return false
 }
+
+function checkIfAlreadyAdded(userArray, addressField) {
+
+    const boolean = userArray.findIndex(checkifPresent);
+
+function checkifPresent(element, addressField) {
+    console.log(element + "checking the element")
+    console.log(addressField + "checking the addressfield")
+
+    if (element == addressField) {
+        return false;
+    }
+    return true; 
+}
+
+return boolean;
+}
+
+
+
+
 
 function codeAddress(address, callback) {
     var array = [];
